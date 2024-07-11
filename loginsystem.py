0efc977d1f5835b8757ede1ab70ad2ee0c610de4
Ammar Rexhaj <167920633+bandyburrito@users.noreply.github.com>
2024-07-11 02:50:22 -0700

    Add files via upload

---
+++ loginsystem.py
@@ -0,0 +1,9 @@
+while True:
+    emri = input("Sheno username: ")
+    if emri == "aa":
+        continue
+    password = input("Shkruaje passwordin: ")
+    if password == "bb":
+        break
+    print("Emri ose Passwordi esht sakte")
+    
